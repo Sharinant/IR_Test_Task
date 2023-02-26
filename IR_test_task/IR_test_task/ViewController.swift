c88c74e554f0891f3cd25b789c861c9a4210e3f7
Sharinant <scharin.an@yandex.ru>
2023-02-26 14:34:59 +0300

    created project

---
+++ IR_test_task/IR_test_task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IR_test_task
+//
+//  Created by Антон Шарин on 26.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
